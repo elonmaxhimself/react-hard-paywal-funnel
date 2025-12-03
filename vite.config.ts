908c673b56fba91f9d969e0e9ad736afd5f989d2
SaigoNoKami <vitk14@gmail.com>
2025-12-03 15:21:57 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -12,16 +12,16 @@ export default defineConfig(({ mode }) => ({
   },
   define: {
     'import.meta.env.VITE_PUBLIC_API_BASE_URL':
-      mode === 'development' ? undefined : 'process.env.VITE_PUBLIC_API_BASE_URL',
+      mode === 'development' ? undefined : JSON.stringify(process.env.VITE_PUBLIC_API_BASE_URL),
     'import.meta.env.VITE_PUBLIC_POSTHOG_TOKEN':
-      mode === 'development' ? undefined : 'process.env.VITE_PUBLIC_POSTHOG_TOKEN',
+      mode === 'development' ? undefined : JSON.stringify(process.env.VITE_PUBLIC_POSTHOG_TOKEN),
     'import.meta.env.VITE_PUBLIC_POSTHOG_HOST':
-      mode === 'development' ? undefined : 'process.env.VITE_PUBLIC_POSTHOG_HOST',
+      mode === 'development' ? undefined : JSON.stringify(process.env.VITE_PUBLIC_POSTHOG_HOST),
     'import.meta.env.VITE_PUBLIC_SHIFT4_PUBLISHABLE_KEY':
-      mode === 'development' ? undefined : 'process.env.VITE_PUBLIC_SHIFT4_PUBLISHABLE_KEY',
+      mode === 'development' ? undefined : JSON.stringify(process.env.VITE_PUBLIC_SHIFT4_PUBLISHABLE_KEY),
     'import.meta.env.VITE_PUBLIC_SHIFT4_PAYMENT_REDIRECT':
-      mode === 'development' ? undefined : 'process.env.VITE_PUBLIC_SHIFT4_PAYMENT_REDIRECT',
+      mode === 'development' ? undefined : JSON.stringify(process.env.VITE_PUBLIC_SHIFT4_PAYMENT_REDIRECT),
     'import.meta.env.VITE_PUBLIC_ENABLE_DEV_ANALYTICS':
-      mode === 'development' ? undefined : 'process.env.VITE_PUBLIC_ENABLE_DEV_ANALYTICS',
+      mode === 'development' ? undefined : JSON.stringify(process.env.VITE_PUBLIC_ENABLE_DEV_ANALYTICS),
   },
 }))

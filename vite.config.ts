44e1acc183817b123a1fe1a8845bd1a0963cac73
SaigoNoKami <vitk14@gmail.com>
2025-12-03 23:00:50 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   },
   
   define: {
-    'import.meta.env.VITE_PUBLIC_POSTHOG_TOKEN': 'process.env.VITE_PUBLIC_POSTHOG_TOKEN',
-    'import.meta.env.VITE_PUBLIC_API_BASE_URL': 'process.env.VITE_PUBLIC_API_BASE_URL',
+    'import.meta.env.VITE_PUBLIC_POSTHOG_TOKEN': JSON.stringify(process.env.VITE_PUBLIC_POSTHOG_TOKEN || ''),
+    'import.meta.env.VITE_PUBLIC_API_BASE_URL': JSON.stringify(process.env.VITE_PUBLIC_API_BASE_URL || ''),
   },
 })

e8f05670cc43aaa67ee7eef5724e0635ab7221af
SaigoNoKami <vitk14@gmail.com>
2025-12-03 22:54:58 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -2,27 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-export default defineConfig(({ mode }) => {
-  console.log('\n=== BUILD TIME DEBUG ===')
-  console.log('Mode:', mode)
-  console.log('NODE_ENV:', process.env.NODE_ENV)
-  
-  console.log('\n--- Your variables via process.env ---')
-  console.log('VITE_PUBLIC_POSTHOG_TOKEN:', process.env.VITE_PUBLIC_POSTHOG_TOKEN)
-  console.log('VITE_PUBLIC_API_BASE_URL:', process.env.VITE_PUBLIC_API_BASE_URL, '1234')
-  
-  console.log('\n--- Default Cloudflare variables ---')
-  console.log('CF_PAGES:', process.env.CF_PAGES)
-  console.log('CF_PAGES_BRANCH:', process.env.CF_PAGES_BRANCH)
-  console.log('========================\n')
-
-  return {
-    plugins: [react()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-        '@@': path.resolve(__dirname, './public'),
-      },
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@@': path.resolve(__dirname, './public'),
     },
-  }
+  },
+  
+  define: {
+    'import.meta.env.VITE_PUBLIC_POSTHOG_TOKEN': 'process.env.VITE_PUBLIC_POSTHOG_TOKEN',
+    'import.meta.env.VITE_PUBLIC_API_BASE_URL': 'process.env.VITE_PUBLIC_API_BASE_URL',
+  },
 })

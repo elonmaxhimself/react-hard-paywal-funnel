71150c2b73751ee9bebf74355e1f82a4f3594008
SaigoNoKami <vitk14@gmail.com>
2025-12-03 05:33:12 +0200

    rework vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@@': path.resolve(__dirname, './public'),
-    },
-  },
-  define: {
-  'import.meta.env': 
-    mode === 'development' 
-      ? 'import.meta.env'
-      : 'process.env',
-}
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
 
-}))
+  const defineConfig = mode === 'production' 
+    ? Object.keys(env)
+        .filter(key => key.startsWith('VITE_PUBLIC_'))
+        .reduce((acc, key) => {
+          acc[`import.meta.env.${key}`] = JSON.stringify(env[key])
+          return acc
+        }, {} as Record<string, string>)
+    : {}
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '@@': path.resolve(__dirname, './public'),
+      },
+    },
+    define: defineConfig,
+  }
+})

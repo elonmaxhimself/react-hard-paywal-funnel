d8c08f529c29548ea8e4cd66695e7e1de68e6620
SaigoNoKami <vitk14@gmail.com>
2026-02-20 11:07:26 +0200

    fix banner language detection

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,13 @@ import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import '@/i18n/config'
+import i18n from '@/i18n/config'
 import App from './App.tsx'
+
+document.documentElement.lang = i18n.language;
+i18n.on('languageChanged', (lng) => {
+    document.documentElement.lang = lng;
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

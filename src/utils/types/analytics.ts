aad60b48a2394eb05b491a62d6a4e7b2b18dd69e
EdiGal91 <47693118+EdiGal91@users.noreply.github.com>
2026-03-14 23:01:09 +0400

    Delete analytics.ts

--- src/utils/types/analytics.ts
+++
@@ -1,9 +0,0 @@
-export interface UTMProps {
-    utm_source?: string;
-    utm_medium?: string;
-    utm_campaign?: string;
-    utm_term?: string;
-    utm_content?: string;
-    deal?: string;
-    [key: string]: string | undefined;
-}
